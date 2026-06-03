1152fabf89dd2ecec550c46101d044d9c72a7c51
amirmhmd485 <amirmhmd378@gmail.com>
2026-06-04 01:53:44 +0300

    Finish Landing Page

---
+++ app.js
@@ -0,0 +1,65 @@
+// start offcanvas
+let settingBtn = document.querySelector(".offcanvas .sun");
+let offcanvas = document.querySelector(".offcanvas");
+let isopen = false;
+function toggleOffcanvas() {
+    if(isopen){
+        offcanvas.classList.remove("open");
+        isopen = false;
+    }
+    else{
+        offcanvas.classList.add("open");
+        isopen = true;
+    }
+}
+settingBtn.addEventListener("click", toggleOffcanvas);
+// end offcanvas
+
+// start BurgerIcon
+let burgerIcon = document.querySelector(".burger-icon");
+let Ul = document.querySelector(".navbar")
+let open = false;
+function toggleUl(){
+    if(open){
+        Ul.classList.remove("open");
+        open = false;
+    }
+    else{
+        Ul.classList.add("open");
+        open = true;
+    }
+}
+burgerIcon.addEventListener("click" , toggleUl);
+// end BurgerIcon
+
+// start random backGround image
+let srcs = ["../images/wallpaperflare.com_wallpaper (1).jpg" , "../images/wallpaperflare.com_wallpaper (2).jpg" , "../images/wallpaperflare.com_wallpaper (3).jpg",
+    "../images/wallpaperflare.com_wallpaper (4).jpg" , "../images/wallpaperflare.com_wallpaper (5).jpg" , "../images/wallpaperflare.com_wallpaper (6).jpg",
+    "../images/wallpaperflare.com_wallpaper (7).jpg" , "../images/wallpaperflare.com_wallpaper (8).jpg" , "../images/wallpaperflare.com_wallpaper (9).jpg",
+    "../images/wallpaperflare.com_wallpaper (10).jpg" , "../images/wallpaperflare.com_wallpaper (11).jpg" , "../images/wallpaperflare.com_wallpaper (12).jpg"
+]
+
+let landingPage = document.querySelector(".landing");
+let randomBtn = document.querySelector(".offcanvas .buttons button.random")
+let stopBtn = document.querySelector(".offcanvas .buttons button.stop")
+
+function randomBackground(){
+    let randomNumber = Math.floor(Math.random() * srcs.length);
+    landingPage.style.backgroundImage = `url("${srcs[randomNumber]}")`;
+}
+let random = setInterval(randomBackground , 10000) 
+randomBtn.addEventListener("click" , function(e){
+    let random = setInterval(randomBackground , 10000) 
+    randomBtn.disabled = true;
+    stopBtn.disabled = false;
+    stopBtn.classList.remove("disabled");
+    this.classList.add("disabled");
+})
+stopBtn.addEventListener("click" , function(e){
+    clearInterval(random);
+    randomBtn.classList.remove("disabled");
+    randomBtn.disabled = false;
+    stopBtn.disabled = true;
+    this.classList.add("disabled");
+})
+// end random backGround image
